13901a8fa0ffcc59e85313802d322bb352c08de2
Justin <jdlow2@gmail.com>
2019-04-24 16:20:31 -0400

    finished ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -5,10 +5,34 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-    // Your code here 
-    
+  // declare pointer f and open the text file
+  FILE *f;
+  f = fopen("text.txt", "a+");
+
+  /* just messing around with file reading */
+  /* printf("%c\n", getc(f)); */
+  /* char a = getc(f); */
+  /* printf("a is %c", a); */
+  /* while((fgetc(c, 1, f) != EOF)) { */
+  /* /1* fgets(storage, 100, f); *1/ */
+  /*   printf("%c", c); */
+  /* } */
+
+  int child = fork();
+  if (child < 0) {
+    printf("error forking\n");
+  } else if (child == 0) {
+    printf("I'm the child, pid %d\n", getpid());
+    char *child_string = "I am the child!!\n";
+    fwrite(child_string, 1, strlen(child_string), f);
+  } else {
+    printf("I'm the parent of child %d, pid %d\n", child, getpid());
+    char *parent_str = "I am the parent!\n";
+    fwrite(parent_str, 1, strlen(parent_str), f);
+  }
     return 0;
 }
